b0d301279e956f8bad53d715785fd7f11ebff944
Amanda Avelino <amavlopes@gmail.com>
2025-05-04 01:44:28 -0300

    schema: modelos e migrations
    define modelos e executa migrations de criação das tabelas

---
+++ prisma/migrations/20250504042912_create_table_departments/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `departments` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `departments_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ prisma/migrations/20250504043056_create_table_allocations/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `allocations` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `day` INTEGER NOT NULL,
+    `startHour` DATETIME(3) NOT NULL,
+    `endHour` DATETIME(3) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
